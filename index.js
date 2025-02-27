438ca4ebd087bff58da011285b65a848c6714b68
sndp bag <sndpbagg@gmail.com>
2025-02-27 19:24:18 +0530

    added client url for cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 // Use middleware
 app.use(cors(
   {
-    origin: "http://localhost:5173", // Allow only your frontend
+    origin: "https://singup-18412.web.app", // Allow only your frontend
     credentials: true // Allow cookies, if needed
   }
 ));
